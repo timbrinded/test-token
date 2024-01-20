e2c2ffa2a408bb771480c283744e13e5c4ad2343
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-01-20 14:12:46 +0000

    initial

--- src/token.sol
+++ src/token.sol
@@ -9,21 +9,19 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUp
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract bGBP is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
+contract stableToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address initialOwner) initializer public {
-        __ERC20_init("bGBP", "bGBP");
+    function initialize(address initialOwner, string memory name_, string memory symbol_) initializer public {
+        __ERC20_init(name_, symbol_);
         __ERC20Burnable_init();
         __ERC20Pausable_init();
         __Ownable_init(initialOwner);
-        __ERC20Permit_init("bGBP");
+        __ERC20Permit_init(name_);
         __UUPSUpgradeable_init();
-
-        _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
     function pause() public onlyOwner {
--- test/token.t.sol
+++ test/token.t.sol
@@ -2,43 +2,67 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {bGBP} from "../src/token.sol";
+import {stableToken} from "../src/token.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
 contract TokenTest is Test {
-    bGBP public token;
+    stableToken public token;
+    bytes4 ERROR_PAUSED = bytes4(keccak256("EnforcedPause()"));
 
     function setUp() public {
         // Deploy the implementation contract
-        bGBP implementation = new bGBP();
+        stableToken implementation = new stableToken();
 
-         // Prepare the initialization call data
-        bytes memory initData = abi.encodeWithSelector(bGBP.initialize.selector, address(this));
+        // Prepare the initialization call data
+        string memory name = "MyStableToken";
+        string memory symbol = "MST";
+        bytes memory initData = abi.encodeWithSelector(
+            stableToken.initialize.selector,
+            address(this), // initialOwner
+            name,
+            symbol
+        );
 
         // Deploy the proxy
         ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initData);
 
         // Cast the proxy to the Benoit interface
-        token = bGBP(address(proxy));
+        token = stableToken(address(proxy));
     }
 
     function test_Name() public {
-        assertEq( token.name(), "bGBP" );
+        assertEq(token.name(), "MyStableToken");
     }
 
     function test_Symbol() public {
-        assertEq( token.symbol(), "bGBP" );
+        assertEq(token.symbol(), "MST");
     }
 
     function test_initialSupply() public {
-        assertEq( token.totalSupply(), 1000 * 10 ** 18 );
+        assertEq(token.totalSupply(), 0);
     }
 
     function test_mintToken() public {
-        token.mint(address(this), 1000 * 10 ** 18);
-        assertEq( token.balanceOf(address(this)), 2000 * 10 ** 18 );
+        uint256 amount = 1000 * 10 ** 18;
+
+        token.mint(address(this), amount);
+        assertEq(token.balanceOf(address(this)), amount);
+        assertEq(token.totalSupply(), amount);
     }
 
-   // test to freeze assets
+    function test_pause() public {
+        token.pause();
+        assertTrue(token.paused());
+    }
+
+    function test_paused() public {
+        token.pause();
+        uint256 amount = 1000 * 10 ** 18;
+
+        vm.expectRevert(ERROR_PAUSED);
+        token.mint(address(this), amount);
+    }
+
+    // test to freeze assets
 }
